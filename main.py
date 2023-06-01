021dcbc4028255bd34a1f3879ab23df0e85c9133
Alqurtubi17 <mohammad.sihabudin.al-2020@ftmm.unair.ac.id>
2023-06-01 11:47:21 +0700

    Testing

---
+++ main.py
@@ -0,0 +1,93 @@
+from flask import Flask, render_template, request
+import cv2
+import numpy as np
+from skimage.feature import hog
+from sklearn.preprocessing import StandardScaler
+import os
+import shutil
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open("model_xgboost_31.pkl", "rb"))
+
+def extract_features(images):
+    features_list = []
+    for img_path in images:
+        img = cv2.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.resize(img, (128, 128))
+        features = hog(img, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2))
+        features_list.append(features)
+    return np.array(features_list)
+
+@app.route('/')
+def home():
+    return render_template('index_coba.html')
+
+    # Get the uploaded images from the request
+    images = request.files.getlist('images')
+
+    # Save the uploaded images to a temporary folder
+    temp_folder = 'temp_images'
+    os.makedirs(temp_folder, exist_ok=True)
+    image_paths = []
+    for img in images:
+        img_path = os.path.join(temp_folder, img.filename)
+        img.save(img_path)
+        image_paths.append(img_path)
+
+    # Extract features from the uploaded images
+    features = extract_features(image_paths)
+
+    # Perform further processing with the extracted features
+    # ...
+
+    # Remove the temporary folder and images
+    shutil.rmtree(temp_folder)
+
+    return 'Image processing completed'
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    images = request.files.getlist('images')
+
+    # Save the uploaded images to a temporary folder
+    temp_folder = 'temp_images'
+    os.makedirs(temp_folder, exist_ok=True)
+    image_paths = []
+    additional_features_list = []
+    for img in images:
+        img_path = os.path.join(temp_folder, img.filename)
+        img.save(img_path)
+        image_paths.append(img_path)
+
+   # Get the additional form values for the current image
+        harga = float(request.form.get('harga'))
+        susunan = float(request.form.get('susunan'))
+        bahan = float(request.form.get('bahan'))
+
+        # Create the additional features array for the current image
+        additional_features = np.array([[harga, susunan, bahan]], dtype=np.float32)
+        additional_features_list.append(additional_features)
+
+    # Extract features from the uploaded images
+    features = extract_features(image_paths)
+
+    # Convert the additional features list to a numpy array
+    additional_features = np.concatenate(additional_features_list, axis=0)
+
+    # Check the shape of the features array
+    if features.shape[0] != additional_features.shape[0]:
+        return 'Feature shape mismatch. Please make sure the number of features extracted from the images matches the expected shape.'
+
+    # Combine the features and additional features arrays
+    combined_features = np.concatenate((features, additional_features), axis=1)
+    # Apply StandardScaler to scale all input features
+    scaler = StandardScaler()
+    scaled_features = scaler.fit_transform(combined_features)
+    
+    prediction = model.predict(scaled_features)
+    return render_template("index_coba.html", prediction_text="{}".format(prediction))
+
+if __name__ == '__main__':
+    app.run(debug=True)
